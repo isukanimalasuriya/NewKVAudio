cf507ae8503d0652c8b7e017e2591d6b578abb8f
isukanimalasuriya <anujitha1234@gmail.com>
2025-03-04 11:25:37 +0530

    dotenv filed created

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import express from "express"
-import bodyParser from "body-parser"
-import mongoose from "mongoose"
-import userRouter from "./routes/userRouter.js"
-import productRouter from "./routes/productRouter.js"
-import jwt from "jsonwebtoken"
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js";
+import productRouter from "./routes/productRouter.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const app = express()
 
@@ -27,7 +30,7 @@ app.use((req, res, next)=>{
     next()
 })
 
-let mongoUrl = "mongodb+srv://isuka:isuka123@cluster0.zicfd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+let mongoUrl = process.env.MONGO_URL
 
 mongoose.connect(mongoUrl)
 const connection = mongoose.connection
